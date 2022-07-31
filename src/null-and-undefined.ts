074d9d9f7ee8430a34d718d99c5e7e383ffc4e54
takna <tak_na@icloud.com>
2022-07-31 21:44:37 +0900

    null, undefined 型

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,13 @@
+export {};
+
+/**
+ * null 型
+ */
+let absence: null = null; // null だけを許容したい場合
+// absence = 'hello'; // null 型でないのでエラー
+
+/**
+ * undefined 型
+ */
+let data: undefined = undefined; // undefined だけを許容したい場合
+// data = 123; // undefined でないのでエラー
