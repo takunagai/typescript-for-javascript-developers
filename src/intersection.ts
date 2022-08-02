b2d2a013dea8ce04b2d610240b24188dffcdb328
takna <tak_na@icloud.com>
2022-08-02 22:35:14 +0900

    unknown 型、intersection 型の修正

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,1 +1,33 @@
 export {};
+
+type Pitcher1 = {
+    throwingSpeed: number,
+};
+
+type Batter1 = {
+    battingAverage: number,
+};
+
+const DaimajinSasaki: Pitcher1 = {
+    throwingSpeed: 154,
+};
+
+const OchiaiHiromitsu: Batter1 = {
+    // throwingSpeed: 154, // throwingSpeed は存在しないので型エラー
+    battingAverage: 0.367,
+};
+
+// どっちもできる選手に throwingSpeed, battingAverage の両方持たせたい
+// これは冗長…
+type TwowayPlayer1 = {
+    throwingSpeed: number,
+    battingAverage: number,
+};
+
+// intersection 型を使うとスッキリ (TwowayPlayer1 と同じ)
+type TwowayPlayer2 = Pitcher1 & Batter1;
+
+const OtaniShouhei: TwowayPlayer2 = {
+    throwingSpeed: 165,
+    battingAverage: 0.286,
+};
--- src/unknown.ts
+++ src/unknown.ts
@@ -1,13 +1,20 @@
 export {};
 
-const kansu = (): number => 43; // 43 を返すだけの関数
+// 43 を返すだけの関数。返り値は number 型
+const kansu = (): number => 43;
 
+// any 型。素通りする
 let numberAny: any = kansu(); // any 型
+let sumAny = numberAny + 10; // 53
+
+// unknown 型
 let numberUnknown: unknown = kansu(); // unknown 型
 
-let sumAny = numberAny + 10; // 53
-// console.log(typeof numberUnknown); // 実行 → number。typeof で、型を判別してくれる
+// let sumUnknown = numberUnknown + 10; // 型エラー(数値じゃないかも知れない)出る。JS なら早期発見できないやつ
+
+console.log(typeof numberUnknown); // → number。typeof で、型を判別できる
+
+// number 型であることが保証されているこのコードなら、エラーが出ない！
 if (typeof numberUnknown === 'number') {
     let sumUnknown = numberUnknown + 10;
 }
-// let sumUnknown = numberUnknown + 10; // 型エラー(数値じゃないかも知れない)。JS なら早期発見できない
