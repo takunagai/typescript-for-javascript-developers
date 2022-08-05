dfbbabed73db174115cf6f17161ce6af36781c2d
takna <tak_na@icloud.com>
2022-08-05 17:57:04 +0900

    getter と setter (アクセサ) 2 - getter

--- src/04_getter-and-setter.ts
+++ src/04_getter-and-setter.ts
@@ -2,10 +2,10 @@ export {};
 
 // 要件
 // * owner (pubric)
-//   - 所有者
-//   - 初期化時に設定できる
+//   - 所有者 ✅
+//   - 初期化時に設定できる ✅
 //   - 途中で変更できない
-//   - 参照できる
+//   - 参照できる ✅
 // * secretNumber (private)
 //   - 個人番号
 //   - 初期化時に設定できる
@@ -13,12 +13,17 @@ export {};
 //   - 参照できない
 
 class MyNumberCard {
-    owner: string;
+    private _owner: string; // 参照のみできるように。getter のメソッド名に owner を使いたいので変数名にアンダースコアを付けた
     secretNumber: number;
 
     constructor(owner: string, secretNumber: number) {
-        this.owner = owner;
+        this._owner = owner;
         this.secretNumber = secretNumber;
+    }
+
+    // 参照するメソッド getter
+    get owner(): string {
+        return this._owner;
     }
 }
 
@@ -26,3 +31,4 @@ class MyNumberCard {
  * インスタンス
  */
 let card = new MyNumberCard('はむさん', 1234567890); //?
+console.log(card.owner);
