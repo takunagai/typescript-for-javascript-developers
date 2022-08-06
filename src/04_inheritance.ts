8d292a6021efdf7687e997d72068dde394ba43ef
takna <tak_na@icloud.com>
2022-08-06 21:55:30 +0900

    継承 02 - コンストラクタをセット(子クラス上書き) ★★課題あり

--- src/04_inheritance.ts
+++ src/04_inheritance.ts
@@ -1,6 +1,8 @@
 export {};
 
 class Animal {
+    constructor(public name: string) {}
+
     run(): string {
         return 'I can run';
     }
@@ -8,13 +10,20 @@ class Animal {
 
 // 継承
 class Lion extends Animal {
+    constructor(public name: string, public speed: number) {
+        super(name); // 継承する子クラスで必要 (super() は、親クラスの同名メソッド。ここではコンストラクタメソッドなので引数に name が必要)
+    } // ★★TODO: 親・子クラス両方でメンバ変数 name を宣言。。子で上書きされるので問題ないが、気持ち悪い
+
     run(): string {
         return 'I can run 80km';
     }
 }
 
-let animal = new Animal();
-console.log(animal.run());
+// let animal = new Animal();
+// console.log(animal.run());
 
-let lion = new Lion();
-console.log(lion.run());
+// let lion = new Lion();
+// console.log(lion.run());
+
+console.log(new Animal('Mickey').run());
+console.log(new Lion('Simba', 80).run()); // 子クラスの使用で2つの引数が必要
